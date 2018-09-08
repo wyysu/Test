960f8c86027e60c95de19ec36aa8b81d3bdd699f
wyysu <43087195+wyysu@users.noreply.github.com>
2018-09-08 19:28:52 +0800

    09081928

--- source/conf.py
+++ source/conf.py
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'wyblog.tex', 'wyblog Documentation',
+    (master_doc, 'wyblog.tex', 'wyblog Doc',
      'Rain', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'wyblog', 'wyblog Documentation',
+    (master_doc, 'wyblog', 'wyblog Doc',
      [author], 1)
 ]
 
@@ -149,7 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'wyblog', 'wyblog Documentation',
+    (master_doc, 'wyblog', 'wyblog Doc',
      author, 'wyblog', 'One line description of project.',
      'Miscellaneous'),
 ]
